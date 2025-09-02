36f8d67ee53eeb2ee312293c25ac725a946d7708
Luca <luca.foglieni.work648@passfwd.com>
2025-09-02 09:10:52 +0200

    balanced nutrition and saturation

--- src/main/java/net/foglia/boiledeggs/items/ModFoods.java
+++ src/main/java/net/foglia/boiledeggs/items/ModFoods.java
@@ -3,5 +3,5 @@ package net.foglia.boiledeggs.items;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public static final FoodProperties BOILEDEGG = new FoodProperties.Builder().nutrition(4).saturationMod(0.6f).build();
+    public static final FoodProperties BOILEDEGG = new FoodProperties.Builder().nutrition(5).saturationMod(0.7F).build();
 }
